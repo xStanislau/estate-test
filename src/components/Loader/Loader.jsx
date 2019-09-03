82a058cb5ebd98cc5db73d106d9c4c6a134e4464
Stanislau Hancharou <stanislau.hancharou@effective-soft.com>
2019-09-03 18:34:17 +0300

    change loader structure

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import { Spinner } from "react-bootstrap";
 import "./Loader.scss";
 
-export default function Loader() {
+export default function Loader({ className }) {
   return (
-    <div className="loader">
-      <Spinner animation="border" variant="primary" />
+    <div className={`loader ${className}`}>
+      <div className="loader__inner">
+        <Spinner animation="border" variant="primary" />
+      </div>
     </div>
   );
 }
